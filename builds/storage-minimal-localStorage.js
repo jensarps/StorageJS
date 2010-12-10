0a2999a0aa812cd770cc37aa58b2d34051fa58c1
Jens Arps <arps@uxebu.com>
2010-12-10 09:59:33 +0100

    New builds, using keepLines=false.

--- builds/storage-minimal-localStorage.js
+++ builds/storage-minimal-localStorage.js
@@ -1,8 +1,1 @@
-var storage={engine:"localStorage",get:function(_1){
-return localStorage.getItem(_1);
-},set:function(_2,_3){
-localStorage.setItem(_2,_3);
-},remove:function(_4){
-localStorage.removeItem(_4);
-}};
-
+var storage={engine:"localStorage",get:function(_1){return localStorage.getItem(_1);},set:function(_2,_3){localStorage.setItem(_2,_3);},remove:function(_4){localStorage.removeItem(_4);}};
--- builds/storage-minimal-widget.js
+++ builds/storage-minimal-widget.js
@@ -1,12 +1,1 @@
-var storage={engine:"widget",get:function(_1){
-return widget.preferenceForKey(_1);
-},set:function(_2,_3){
-this.ensureKeyInMap(_2);
-widget.setPreferenceForKey(_3,_2);
-},remove:function(_4){
-this.removeKeyFromMap(_4);
-widget.setPreferenceForKey(null,_4);
-},ensureKeyInMap:function(){
-},removeKeyFromMap:function(){
-}};
-
+var storage={engine:"widget",get:function(_1){return widget.preferenceForKey(_1);},set:function(_2,_3){this.ensureKeyInMap(_2);widget.setPreferenceForKey(_3,_2);},remove:function(_4){this.removeKeyFromMap(_4);widget.setPreferenceForKey(null,_4);},ensureKeyInMap:function(){},removeKeyFromMap:function(){}};
